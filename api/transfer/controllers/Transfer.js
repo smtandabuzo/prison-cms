dbec180b534d11f82f134726b990c255639ab12c
Sazi <s_mtandabuzo@yahoo.com>
2019-08-06 20:57:42 +0200

    Transfer API files

---
+++ api/transfer/controllers/Transfer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Read the documentation () to implement custom controller functions
+ */
+
+module.exports = {
+  nationalID: async function(params) {
+    let url = params.request.url;
+    let length = url.slice(18).length;
+    console.log('Length ' + length);
+    let param = url.slice(-(length));
+    console.log('Param ' + param);
+    return await Transfer.find({nationalID: param});
+  },
+  deleteByNationalID: async function(params){
+    let url = params.request.url;
+    let length = url.slice(18).length;
+    console.log('Length ' + length);
+    let param = url.slice(-(length));
+    return await Transfer.deleteOne({nationalID: param});
+  },
+};
